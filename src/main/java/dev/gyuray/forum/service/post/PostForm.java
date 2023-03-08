27a61516718fddb9c6d53bb6764029dff0198f96
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-08 17:23:25 +0900

    Add PostForm class

---
+++ src/main/java/dev/gyuray/forum/service/post/PostForm.java
@@ -0,0 +1,10 @@
+package dev.gyuray.forum.service.post;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class PostForm {
+    private String title;
+    private String content;
+}
