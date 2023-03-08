73c54ab99b88a3e96b28cc63d2a49e6ffd4931a5
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-08 15:44:01 +0900

    Add UserRepository Class

---
+++ src/main/java/dev/gyuray/forum/repository/user/UserRepository.java
@@ -0,0 +1,50 @@
+package dev.gyuray.forum.repository.user;
+
+import dev.gyuray.forum.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class UserRepository {
+
+    private final EntityManager em;
+
+    public User save(User user) {
+        em.persist(user);
+        return user;
+    }
+    public Optional<User> findOne(Long userId) {
+        return Optional.ofNullable(em.find(User.class, userId));
+    }
+
+    public void update(UserUpdateDTO userUpdateDTO) {
+        User user = em.find(User.class, userUpdateDTO.getId());
+        user.setAddress(userUpdateDTO.getAddress());
+        user.setRole(userUpdateDTO.getRole());
+        user.setPosts(userUpdateDTO.getPosts());
+        user.setComments(userUpdateDTO.getComments());
+    }
+
+    public Optional<User> findByName(String name) {
+        return Optional.ofNullable(
+                em.createQuery("select u from User u where user_name = :name", User.class)
+                .setParameter("name", name)
+                .getSingleResult());
+    }
+
+    public List<User> findAll() {
+        return em.createQuery("select u from User")
+                .getResultList();
+    }
+
+    public void delete(Long userId) {
+        User foundUser = em.find(User.class, userId);
+        em.remove(foundUser);
+    }
+
+}
