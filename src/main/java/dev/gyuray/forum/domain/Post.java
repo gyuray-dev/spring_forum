15097d2b7ad83e4a54ead051992c7a118a67db2e
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-08 11:50:11 +0900

    Add column name to id fields for Entity Classes

--- src/main/java/dev/gyuray/forum/domain/Post.java
+++ src/main/java/dev/gyuray/forum/domain/Post.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class Post {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="post_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
