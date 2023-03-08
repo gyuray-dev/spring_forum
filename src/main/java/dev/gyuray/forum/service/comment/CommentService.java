e1da4696f287b47502479f8cee91f57957f16050
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-08 17:46:58 +0900

    Add CommentService class

---
+++ src/main/java/dev/gyuray/forum/service/comment/CommentService.java
@@ -0,0 +1,33 @@
+package dev.gyuray.forum.service.comment;
+
+import dev.gyuray.forum.domain.Comment;
+import dev.gyuray.forum.repository.comment.CommentRepository;
+import dev.gyuray.forum.repository.comment.CommentUpdateDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CommentService {
+
+    private final CommentRepository commentRepository;
+
+    public void comment(CommentForm commentForm) {
+        Comment comment = new Comment();
+        comment.setContent(commentForm.getContent());
+        commentRepository.save(comment);
+    }
+
+    @Transactional
+    public void updateComment(CommentUpdateDTO commentUpdateDTO) {
+        commentRepository.update(commentUpdateDTO);
+    }
+
+    @Transactional
+    public void deleteComment(Long commentId) {
+        commentRepository.delete(commentId);
+    }
+
+}
