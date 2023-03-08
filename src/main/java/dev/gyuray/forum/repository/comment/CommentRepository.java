72df09c8a4cb672ff407d83d5e8ad70b1b7d58ef
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-08 16:27:28 +0900

    Add CommentRepository Class

---
+++ src/main/java/dev/gyuray/forum/repository/comment/CommentRepository.java
@@ -0,0 +1,40 @@
+package dev.gyuray.forum.repository.comment;
+
+
+import dev.gyuray.forum.domain.Comment;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class CommentRepository {
+
+    private final EntityManager em;
+
+    public Comment save(Comment comment) {
+        em.persist(comment);
+        return comment;
+    }
+    public Optional<Comment> findOne(Long commentId) {
+        return Optional.ofNullable(em.find(Comment.class, commentId));
+    }
+
+    public void update(CommentUpdateDTO commentDTO) {
+        Comment foundComment = em.find(Comment.class, commentDTO.getId());
+        foundComment.setContent(commentDTO.getContent());
+    }
+
+    public List<Comment> findAll() {
+        return em.createQuery("select c from Comment c")
+                .getResultList();
+    }
+
+    public void delete(Long commentId) {
+        Comment foundComment = em.find(Comment.class, commentId);
+        em.remove(foundComment);
+    }
+}
