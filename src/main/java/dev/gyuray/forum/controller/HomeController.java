3cc9b8e8bf5c3aec2a60cfdcb475cfe266a5ef6d
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-10 16:18:54 +0900

    Add post listing feature to HomeController.java

--- src/main/java/dev/gyuray/forum/controller/HomeController.java
+++ src/main/java/dev/gyuray/forum/controller/HomeController.java
@@ -1,13 +1,25 @@
 package dev.gyuray.forum.controller;
 
+import dev.gyuray.forum.domain.Post;
+import dev.gyuray.forum.repository.post.PostListDTO;
+import dev.gyuray.forum.service.PostService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final PostService postService;
+    
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        List<PostListDTO> posts = postService.findAll();
+        model.addAttribute("posts", posts);
         return "index";
     }
 }
