7f45a501c4fda9ca4b49f06e76b388893493dd99
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-10 16:24:09 +0900

    Add PostServiceTest.findAll()

--- src/test/java/dev/gyuray/forum/service/PostServiceTest.java
+++ src/test/java/dev/gyuray/forum/service/PostServiceTest.java
@@ -2,20 +2,28 @@ package dev.gyuray.forum.service;
 
 import dev.gyuray.forum.domain.Post;
 import dev.gyuray.forum.repository.post.PostForm;
+import dev.gyuray.forum.repository.post.PostListDTO;
 import dev.gyuray.forum.repository.post.PostUpdateDTO;
-import dev.gyuray.forum.service.PostService;
+import dev.gyuray.forum.repository.user.UserForm;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+
+import java.util.List;
+
 @SpringBootTest
 @Transactional
+@Slf4j
 class PostServiceTest {
 
     @Autowired
     PostService postService;
+    @Autowired
+    UserService userService;
 
     @Test
     void basicCrud() {
@@ -45,5 +53,28 @@ class PostServiceTest {
         Assertions.assertThrows(IllegalStateException.class, () -> {
             postService.findPostById(postId);
         });
+    }
+
+    @Test
+    void findAll() {
+        // writer
+        UserForm userFormA = new UserForm();
+        userFormA.setName("userA");
+        Long userId = userService.join(userFormA);
+        log.info("userId = {}", userId);
+
+        // post
+        PostForm postForm1 = new PostForm();
+        postForm1.setUserId(userId);
+        postForm1.setTitle("제목1");
+        PostForm postForm2 = new PostForm();
+        postForm2.setUserId(userId);
+        postForm2.setTitle("제목2");
+
+        Long postId1 = postService.post(postForm1);
+        Long postId2 = postService.post(postForm2);
+        List<PostListDTO> postListDTOs = postService.findAll();
+
+        Assertions.assertEquals(2, postListDTOs.size());
     }
 }
