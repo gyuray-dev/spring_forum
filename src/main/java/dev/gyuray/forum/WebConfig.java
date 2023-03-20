57e52c1e7572c8308eca41dc5b8c43be2acfc4e6
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-20 09:14:17 +0900

    Rename LoginInterceptor.java to LoginButtonsInterceptor.java

--- src/main/java/dev/gyuray/forum/WebConfig.java
+++ src/main/java/dev/gyuray/forum/WebConfig.java
@@ -1,6 +1,6 @@
 package dev.gyuray.forum;
 
-import dev.gyuray.forum.controller.LoginInterceptor;
+import dev.gyuray.forum.controller.LoginButtonsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new LoginInterceptor())
+        registry.addInterceptor(new LoginButtonsInterceptor())
                 .order(1)
                 .addPathPatterns("/**");
     }
--- src/main/java/dev/gyuray/forum/controller/LoginButtonsInterceptor.java
+++ src/main/java/dev/gyuray/forum/controller/LoginButtonsInterceptor.java
@@ -1,7 +1,5 @@
 package dev.gyuray.forum.controller;
 
-import dev.gyuray.forum.domain.User;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -9,7 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class LoginInterceptor implements HandlerInterceptor {
+public class LoginButtonsInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) {
