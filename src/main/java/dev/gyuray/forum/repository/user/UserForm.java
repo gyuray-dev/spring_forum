73107ddbbf4dd6c6bc4995e13c84e8c8d4eafa80
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-21 13:38:03 +0900

    Add Bean Validation to UserForm

--- src/main/java/dev/gyuray/forum/repository/user/UserForm.java
+++ src/main/java/dev/gyuray/forum/repository/user/UserForm.java
@@ -2,21 +2,28 @@ package dev.gyuray.forum.repository.user;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.lang.Nullable;
 
-import javax.persistence.Column;
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.*;
 
 @Getter @Setter
 public class UserForm {
-    @NotBlank
+    @Size(min = 2, max = 12, message = "4~12자 범위로 입력해주세요.")
     private String name;
+
     private String city;
     private String street;
+
+    @Size(max = 12, message = "12자 이하로 입력해주세요.")
     private String zipcode;
+
     @NotBlank
+    @Email
     private String email;
-    @NotBlank
+
+    @Pattern(regexp = "[a-zA-Z1-9]{4,16}", message = "아이디는 영문자 및 숫자 4~16자리로 입력해주세요.")
     private String loginId;
-    @NotBlank
+
+    @Size(min = 4, max = 16, message = "4~16자 범위로 입력해주세요.")
     private String password;
 }
