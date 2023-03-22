6939526594fdfa06adcd2568252c14d3937d2a01
gyuray-dev <choieungyu.dev@gmail.com>
2023-03-22 15:42:13 +0900

    Add image upload feature for TOAST UI Editor

--- src/main/java/dev/gyuray/forum/controller/FileController.java
+++ src/main/java/dev/gyuray/forum/controller/FileController.java
@@ -3,17 +3,18 @@ package dev.gyuray.forum.controller;
 import dev.gyuray.forum.domain.UploadFile;
 import dev.gyuray.forum.repository.post.UploadFileRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.util.UriUtils;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.charset.StandardCharsets;
 
@@ -23,6 +24,7 @@ import java.nio.charset.StandardCharsets;
 public class FileController {
 
     private final UploadFileRepository uploadFileRepository;
+    private final FileManager fileManager;
 
     @Value("${file.path}")
     private String filePath;
@@ -46,5 +48,29 @@ public class FileController {
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, contentDisposition)
                 .body(resource);
+    }
+
+    @ResponseBody
+    @GetMapping("/images/{storedFileName}")
+    public Resource downloadImage(
+            @PathVariable String storedFileName
+    ) throws MalformedURLException {
+        return new UrlResource("file:" + filePath + storedFileName);
+    }
+
+    @ResponseBody
+    @PostMapping("/images/new")
+    public String uploadImage(
+            @RequestParam("image") MultipartFile multipartFile
+    ) throws IOException {
+
+        if (multipartFile.isEmpty()) {
+            return "not found";
+        }
+
+        UploadFile uploadFile = fileManager.storeFile(multipartFile);
+        String storedFileName = uploadFile.getStoredFileName();
+
+        return "/files/images/" + storedFileName;
     }
 }
